41086fff47506882393702c5fe8d6fcfad0659d7
AlanBezerra20 <qalan711@gmail.com>
2026-01-07 11:56:14 -0300

    Tabuada

---
+++ aula13e14/javascripttb.js
@@ -0,0 +1,18 @@
+function GerarTabuada(){
+    let txtnum = Number(document.getElementById("txtnum").value)
+    let msg = document.getElementById("msg")
+    msg.innerHTML= "Calculando... <br>";
+if (txtnum==0){
+    window.alert("[ERRO] Digite um numero válido!")
+}else{
+    let num= txtnum;
+    num= Number(txtnum);
+   
+   for( sub=0; sub<=10; sub++){
+    let res = num * sub;
+    msg.innerHTML += `${num} x ${sub} = ${res} <br>`;
+    
+   }
+}
+
+}
