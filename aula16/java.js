e4e4085df20916d7cf08657adb65a72919935cce
AlanBezerra20 <qalan711@gmail.com>
2026-01-14 20:56:16 -0300

    comecei o desafio 20

---
+++ aula16/java.js
@@ -0,0 +1,34 @@
+/*
+
+Exemplo 1: 
+function parouimpar(n) {
+    if (n % 2 === 0) {
+        return "par";
+    } else {
+        return "impar";
+    }
+}
+let res= parouimpar(22);
+alert(`O número é ${res}`)
+*/
+
+
+/*
+  Exemplo 2:  
+let v = function(x) {
+    return x * 5;
+}
+alert(v(8));
+*/
+
+
+
+// Exemplo 3:
+function fatorial(n){
+    let fat = 1;
+    for(let c= n; c>1; c--){
+        fat*=c;
+    }
+    return fat;
+}
+alert(fatorial(5));
