def94361fadfb7e6757d2cf48e10229979bce27e
AlanBezerra20 <qalan711@gmail.com>
2026-01-15 11:00:36 -0300

    contiuaçao do desafio

--- aula16/desafio.js
+++ aula16/desafio.js
@@ -1,4 +1,17 @@
-Adicionar(){
+ function Adicionar(){
+let num = []
+num.push(Number(document.getElementById('txtnum').value))
+let select = document.getElementById('fim')
+let finum = document.createElement('finum')
 
-    
+if(num < 1 || num >100 || num===0){
+    window.alert("Número inválido ! TENTE NOVAMENTE.")
+} else{
+    finum.text=`Valor ${num}adicionado.`
+  
+}
+
+
+
+
 }
