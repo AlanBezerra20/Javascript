a8effb5f95acb20a0211a889516d2895f9dbdf10
AlanBezerra20 <qalan711@gmail.com>
2026-01-15 18:52:28 -0300

    desafio20

--- aula16/desafio.js
+++ aula16/desafio.js
@@ -1,17 +1,52 @@
+ 
+ let num= document.querySelector('input#txtnum')
+ let lista= document.querySelector('select#flista')
+let finum= document.querySelector('div#res')
+valor =[]
+ 
+ 
  function Adicionar(){
-let num = []
-num.push(Number(document.getElementById('txtnum').value))
-let select = document.getElementById('fim')
-let finum = document.createElement('finum')
+    let n = Number(num.value)
 
-if(num < 1 || num >100 || num===0){
-    window.alert("Número inválido ! TENTE NOVAMENTE.")
-} else{
-    finum.text=`Valor ${num}adicionado.`
+    if (n < 1 || n > 100 || valor.includes(n)) {
+        window.alert('Número inválido ou já encontrado na lista!')
+    } else {
+        valor.push(n)
+    }
+    let item = document.createElement('option')
+        item.innerHTML = `Valor ${n} adicionado`
+        lista.appendChild(item)
+
+        finum.innerHTML = ''
+}
   
+    function Finalizar(){
+        if(valor.length==0){
+            window.alert('Adicione valores antes de finalizar !')
+        }
+        let total = valor.length
+        let maior = valor[0]
+        let menor = valor[0]
+        let soma = 0
+
+
+for (let pos in valor) {
+    soma += valor[pos]
+
+    if (valor[pos] > maior)
+        maior = valor[pos]
+
+    if (valor[pos] < menor)
+        menor = valor[pos]
 }
+let media = soma / total
+finum.innerHTML = ''
+finum.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+finum.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+finum.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+finum.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+finum.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
 
 
+    }
 
-
-}
